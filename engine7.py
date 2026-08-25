eacf87ee283522266909dee361c8b6f647da6880
dennykusumah <dennykusumah@gmail.com>
2026-08-25 18:32:25 +0700

    Update engine7.py

--- engine7.py
+++ engine7.py
@@ -465,4 +465,3 @@ class InfoPendukungEngine:
         except Exception as e:
             import traceback
             return False, f'InfoPendukungEngine Error: {str(e)}\n{traceback.format_exc()}'
-    
